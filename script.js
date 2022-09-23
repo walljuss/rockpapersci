68f8061123d3a111baad22a2fb766ef7e4b0c2f4
walljuss <walljuss@gmail.com>
2022-09-23 19:56:10 +0600

    almost done except the design

--- script.js
+++ script.js
@@ -4,61 +4,84 @@ const getComputerChoice = () => {
   return choices[loc];
 }
 
-const playRound = (playerSelection, computerSelection) => {
-  if (playerSelection === computerSelection) {
-    return "It's a tie!"
-  } else if (playerSelection === 'rock') {
-    if (computerSelection === 'paper'){
-      return 'You lost!';
+const game = (computerPick, playerPick, computerscore, playerscore, live) => {
+  let i;
+  if (computerPick.textContent === playerPick.textContent) {
+    live.textContent = "It's a tie!"
+  } else if (computerPick.textContent === 'rock') {
+    if (playerPick.textContent === 'paper') {
+      live.textContent = 'You won!'
+      i =  parseInt(playerscore.textContent);
+      i++;
+      playerscore.textContent = i;
     } else {
-      return 'You won!';
+      live.textContent = 'You lost!'
+      i = parseInt(computerscore.textContent);
+      i++;
+      computerscore.textContent = i;
     }
-  } else if (playerSelection === 'scissors') {
-    if (computerSelection === 'paper') {
-      return 'You won!';
+  } else if (computerPick.textContent === 'paper') {
+    if (playerPick.textContent === 'scissors') {
+      live.textContent = 'You won!'
+      i = parseInt(playerscore.textContent);
+      i++;
+      playerscore.textContent = i;
     } else {
-      return 'You lost!';
-    }
+      live.textContent = 'You lost!'
+      i = parseInt(computerscore.textContent);
+      i++;
+      computerscore.textContent = i;
+    } 
   } else {
-    if (computerSelection === 'rock') {
-      return 'You won!';
+    if (playerpick.textContent === 'paper') {
+      live.textContent = 'You lost!';
+      i = parseInt(computerscore.textContent);
+      i++;
+      computerscore.textContent = i;
     } else {
-      return 'You lost!';
+      live.textContent = 'You win!';
+      i =  parseInt(playerscore.textContent);
+      i++;
+      playerscore.textContent = i;
     }
   }
 }
 
-const game = () => {
-  let computerscore = 0;
-  let playerscore = 0;
-  console.log("Let the game begin! You'll be playing 5 rounds of rock, paper, scissors\
-  with computer!");
-  for (let i = 0; i<5; i++){
-  
-    let playerPick = prompt("Pick one!");
-    playerPick = playerPick.toLowerCase();
-    if ( (playerPick !== 'rock') && (playerPick !== 'paper') && 
-    (playerPick !== 'scissors') ) {
-      console.log('Please follow the game rules pick: \
-       paper, rock or scissors! You lost this round');
-       computerscore++;
-       console.log(`Round ${i+1} \n Computer: ${computerscore} - Player: ${playerscore}`);
-       continue;
-    }
-    let result = playRound(playerPick, getComputerChoice());
-    if (result === 'You won!') {
-      playerscore++;
-    } else if (result === 'You lost!') {
-      computerscore++;
-    } 
-    console.log(result);
-    console.log(`Round ${i+1} \n Computer: ${computerscore} - Player: ${playerscore}`);
-  }
-  if (computerscore > playerscore) {
-    console.log('Unfortunately, computer won the game!')
-  } else if (playerscore > computerscore ){
-    console.log('Congratulations, you won the game!')
-  } else {
-    console.log('The game is a tie!')
-  }
- }
+ /* As player presses each image the variable choice saves the paper, rock, or scissors
+ in choice variable */
+
+ const playerpick = document.querySelector('.playerpick');
+ const computerpick = document.querySelector('.computerpick');
+ const comppoint = document.querySelector('.computerpoint');
+ const playpoint = document.querySelector('.playerpoint');
+ const livetext = document.querySelector('.live');
+ let choice;
+
+ const paper = document.getElementById('paper');
+ paper.addEventListener('click', () => {
+  choice = 'paper';
+  playerpick.textContent = choice;
+  computerpick.textContent = getComputerChoice();
+  game(computerpick, playerpick, comppoint, playpoint, livetext);
+ })
+
+ const scissors = document.getElementById('scissors');
+ scissors.addEventListener('click', () =>{
+  choice = 'scissors';
+  playerpick.textContent = choice;
+  computerpick.textContent = getComputerChoice();
+  game(computerpick, playerpick, comppoint, playpoint, livetext);
+
+ })
+
+ const rock = document.getElementById('rock');
+ rock.addEventListener('click', () => {
+  choice = 'rock';
+  playerpick.textContent = choice;
+  computerpick.textContent = getComputerChoice();
+  game(computerpick, playerpick, comppoint, playpoint, livetext);
+ })
+
+
+
+
